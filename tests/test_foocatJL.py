829bd9172f87798f6478f12b99d2bd8fad464a7e
Jarome Leslie <jsleslie@users.noreply.github.com>
2020-03-01 21:11:47 -0400

    added unit tests for catbind

--- tests/test_foocatJL.py
+++ tests/test_foocatJL.py
@@ -1,1 +1,11 @@
 from foocatJL import foocatJL
+
+import pandas as pd
+
+
+def test_catbind():
+    a = pd.Categorical(["character", "hits", "your", "eyeballs"])
+    b = pd.Categorical(["but", "integer", "where it", "counts"])
+    assert ((foocatJL.catbind(a, b)).codes == [1, 4, 7, 3, 0, 5, 6, 2]).all()
+    assert ((foocatJL.catbind(a, b)).categories == ["but", "character",
+            "counts", "eyeballs", "hits", "integer", "where it", "your"]).all()
